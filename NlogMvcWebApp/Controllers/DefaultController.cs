7f79182485faec31de889afef00d68c7c61f26c9
oblily <business.mail.kl@gmail.com>
2016-01-30 21:39:13 +0900

    Create ASP.NET MVC applications using Visual Studio 2015 and Nlog

---
+++ NlogMvcWebApp/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NlogMvcWebApp.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+            logger.Debug("Debug Logging test");
+            logger.Info("Info Logging test");
+            logger.Warn("Warn Logging test");
+            logger.Error("Error Logging test");
+            logger.Fatal("Fatal Logging test");
+
+            return View();
+        }
+    }
+}
